37cd6b6c69fc1b207291f4c6cbd768a8dc7d8381
Mitch Olson <mitch3344@gmail.com>
2021-10-15 08:52:21 -0500

    completed battlefield

--- Main.py
+++ Main.py
@@ -1,40 +1,67 @@
+from robot import Robot
+
 class Fleet:
     def __init__(self):
-        self.robots = ""
-    def create_fleet():
-        pass
+        self.robots = []
+    def create_fleet(self):
+        robot1 = Robot('tx100')
+        robot2 = Robot('tx200')
+        robot3 = Robot('tx300')
+        self.robots.append(robot1)
+        self.robots.append(robot2)
+        self.robots.append(robot3)
 
+    from weapon import Weapon
 
 
 class Robot:
-    def __init__(self):
-        self.name = []
-        self.health = ()
-        self.weapon = ""
-    def attack(self):
-        self.dinosaur = ()
-
-
+    def __init__(self,robot_name):
+        self.name = 'name'
+        self.health = 100
+        self.weapon = Weapon()
+    
+    def attack(self, dinosaur):
+        if dinosaur.health >= 0:
+            dinosaur.health = dinosaur.health - self.attack_power
+            print(f'{self.name} delivers {self.weapon.attack_power} damage to {dinosaur.name}')
+            print(f'{dinosaur.name} weakend to {dinosaur.health} health')
+        else:
+            print("Deafeated!")
 
 class Weapon:
-    def __init__(self):
-        self.name = ""
-        self.attack_power = ()
+    def __init__(self,):
+        self.name = 'rockets'
+        self.attack_power = 25
 
-
+    from dinosaur import Dinosaur
 
 class Herd:
     def __init__(self):
         self.dinosaurs = []
     def create_herd(self):
+        dinosaur1 = Dinosaur('Steg')
+        dinosaur2 = Dinosaur('Velo')
+        dinosaur3 = Dinosaur('Tea')
+        self.dinosaurs.append(dinosaur1)
+        self.dinosaurs.append(dinosaur2)
+        self.dinosaurs.append(dinosaur3)
+
 
 
 
 class Dinosaur:
-    def __init__(self):
-        self.health = ()
-        self.name = ""
-        self.attack_power = ()
+    def __init__(self,dinosaur_name):
+        self.name = "name"
+        self.health = 100
+        self.attack_power = (25)
     
-    def attack(self):
+    def attack(self,robot):
+        if robot.health >= 0:
+            robot.health = robot.health - self.attack_power
+            print(f'{self.name} delivers {self.attack_power} damgage to {robot.name}')
+            print(f'{robot.name} weakend to {robot.health} health')
+        else:
+            print('Defeated!')
+
+    
         
--- battlefield.py
+++ battlefield.py
@@ -1,5 +1,8 @@
-from Main import Fleet, Herd
 
+from Main import Fleet
+from Main import Herd
+
+import random
 
 class Battlefield:
     def __init__(self):
@@ -7,25 +10,55 @@ class Battlefield:
         self.herd = Herd()
 
     def run_game(self):
-        pass
+        self.display_welcome()
+        self.display_winners(self.battle)()
 
+    
     def display_welcome(self):
-        pass
+        print('Robots vs Dinosaurs')
 
+    
     def battle(self):
-        pass
+        # method to pick robot and dinosaur  
+        robots_defeated = False
+        dinosaurs_defeated = False
+        rotate_count = 1
+        while robots_defeated != True and dinosaurs_defeated != True:
+            self.robot(random.choice(self.fleet.robots))
+            dinosaurs_defeated = self.show_dino_opponent_options()
+            self.dinosaur(random.choice(self.herd.dinosaurs))
+            robots_defeated = self.show_robo_opponent_options()
+            print(f'rotate {rotate_count}')
+            rotate_count +=1
+        if robots_defeated == True:
+            return 'Raawr!'
+        if dinosaurs_defeated == True:
+            return 'Beep boop, Robots win!'
 
-    def dino_turn(self, robot):
-        pass
+
+       
+    def dino_turn(self, dinosaur):
+        dinosaur.attack(random.choice(self.fleet.robots))
 
     def robo_turn(self,robot):
-        pass
+        robot.attack(random.choice(self.herd.dinosaurs))
+        
     
     def show_dino_opponent_options(self):
-        pass
-
+        print(f'{self.herd.dinosaurs[0].name} has {self.herd.dinosaurs[0].health}.health')
+        print(f'{self.herd.dinosaurs[1].name} has {self.herd.dinosaurs[1].health}.health')
+        print(f'{self.herd.dinosaurs[2].name} has {self.herd.dinosaurs[2].health}.health')
+        if self.herd.dinosaurs[0].health <= 0 and self.herd.dinosaurs[1].health <= 0 and self.heard.dinosaurs[2].health <= 0:           
+            return True
+  
     def show_robo_opponent_options(self):
-        pass 
+        print(f'{self.fleet.robots[0].name} has {self.fleet.robots[0].health}.health')
+        print(f'{self.fleet.robots[1].name} has {self.fleet.robots[1].health}.health')
+        print(f'{self.fleet.robots[2].name} has {self.fleet.robots[2].health}.health')
+        if self.fleet.robots[0].health <= 0 and self.fleet.robots[1].health <= 0 and self.fleet.robots[2].health <= 0:
+            return True
 
-    def display_winners(self):
-        pass
+    
+    
+    def display_winners(self,Win):
+        print(Win)
